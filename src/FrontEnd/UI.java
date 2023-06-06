2befc71a290fbc10ebfa2d35ad7b6f6d8d8b6228
Amaya <amaya.hallim@gmail.com>
2023-06-06 13:56:07 -0400

    MenuBar

--- src/FrontEnd/UI.java
+++ src/FrontEnd/UI.java
@@ -29,77 +29,65 @@ public class UI extends JFrame implements ActionListener {
     public void mainWindow(){
         setSize(500,300);
         setVisible(true);
-        //setLayout(new GridLayout(2,1));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        GridLayout lay = new GridLayout(2, 1);
+        setLayout(new GridLayout(2, 1));
 
 
-        JPanel p1 = new JPanel();
-        p1.setBackground(Color.BLUE);
+        JMenuBar mb = new JMenuBar();
+        JMenu booking, guest, manual, cancel;
+        JMenuItem ticketC, planeC;
 
-        JPanel BIG = new JPanel();
-        BIG.add(p1);
-        BIG.add(p2);
-        BIG.setLayout(lay);
-        add(BIG);
+        booking = new JMenu("Book Ticket");
+        booking.addActionListener(this);
+        booking.setActionCommand("Book");
 
-        JButton b1 = new JButton("go to booking");
-        b1.addActionListener(this);
-        b1.setActionCommand("Book");
-        b1.setSize(20,10);
-        p1.add(b1);
+        guest = new JMenu("Flight Info");
+        guest.addActionListener(this);
+        guest.setActionCommand("Guest");
 
-        JButton b2 = new JButton("go to guest");
-        b2.addActionListener(this);
-        b2.setActionCommand("Guest");
-        p1.add(b2);
+        manual = new JMenu("User Manual");
+        manual.addActionListener(this);
+        manual.setActionCommand("Manual");
 
-        JButton b3 = new JButton("go to Manuel");
-        b3.addActionListener(this);
-        b3.setActionCommand("Man");
-        p1.add(b3);
+        cancel = new JMenu("Cancellations");
+        cancel.addActionListener(this);
+        cancel.setActionCommand("Cancel");
 
-        JButton b4 = new JButton("go to Cancel");
-        b4.addActionListener(this);
-        b4.setActionCommand("Can");
-        p1.add(b4);
+        ticketC = new JMenuItem("Cancel Ticket");
+        ticketC.addActionListener(this);
+        ticketC.setActionCommand("TicketCancel");
 
+        planeC = new JMenuItem("Cancel Flight");
+        planeC.addActionListener(this);
+        planeC.setActionCommand("FlightCancel");
+
+
+        mb.add(booking);
+        mb.add(guest);
+        mb.add(manual);
+        mb.add(cancel);
+        cancel.add(ticketC);
+        cancel.add(planeC);
+        setJMenuBar(mb);
+        add(p2);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String sE = e.getActionCommand();
+
         switch (sE) {
-            case "Book" : BookingScreen();
+            case "Book" :
+                //BookingScreen();
+                System.out.println("hi");
             break;
             case "Guest" : System.out.println(PlaneDB.isSeatBooked("201", 2));
             break;
-            case "Man" : DisplayManuel();
+            case "Manual" : DisplayManuel();
             break;
-            case "Can" : System.out.println(PlaneDB.isSeatBooked("201", 6));
+            case "Cancel" : System.out.println(PlaneDB.isSeatBooked("201", 6));
             break;
             default : System.out.println("something went wrong");
-        }
-    }
-
-    /**
-     * Switch window.
-     *
-     * @param whatWin the  window it is switching to
-     */
-    public void SwitchWindow(String whatWin){
-        switch (whatWin){
-            case "main": mainWindow();
-            break;
-            case "booking": BookingScreen();
-            break;
-            case "can": DisplayCancellation();
-            break;
-            case "man": DisplayManuel();
-            break;
-            case "guest": GuestInfo();
-            break;
-            default: mainWindow();
         }
     }
 
@@ -108,6 +96,7 @@ public class UI extends JFrame implements ActionListener {
      * Booking screen.
      */
     public void BookingScreen(){
+
         p2.setBackground(Color.BLACK);
     }
 
