1b585173ff799b83b0c2b9861317617740ea1efc
Amaya <amaya.hallim@gmail.com>
2023-06-07 11:19:08 -0400

    Trying to add functionaity to guest info in menu bar

--- src/FrontEnd/UI.java
+++ src/FrontEnd/UI.java
@@ -31,37 +31,29 @@ public class UI extends JFrame implements ActionListener {
         setSize(500,300);
         setVisible(true);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(1, 1));
 
 
         JMenuBar mb = new JMenuBar();
-        //JButton booking, guest, manual;
         JMenu cancel;
         JMenuItem booking, guest, manual, ticketC, planeC;
 
-        //Border emptyBorder = BorderFactory.createEmptyBorder();
 
         booking = new JMenuItem("Book Ticket ");
-        //booking.setBackground(Color.WHITE);
-        //booking.setBorder(emptyBorder);
         booking.addActionListener(this);
         booking.setActionCommand("Book");
 
         guest = new JMenuItem("Flight Info ");
-        //guest.setBackground(Color.WHITE);
-        //guest.setBorder(emptyBorder);
         guest.addActionListener(this);
         guest.setActionCommand("Guest");
 
         manual = new JMenuItem("User Manual ");
-        //manual.setBackground(Color.WHITE);
-        //manual.setBorder(emptyBorder);
         manual.addActionListener(this);
         manual.setActionCommand("Manual");
 
         cancel = new JMenu("Cancellations");
-        cancel.addActionListener(this);
-        cancel.setActionCommand("Cancel");
+        //cancel.addActionListener(this);
+        //cancel.setActionCommand("Cancel");
 
         ticketC = new JMenuItem("Cancel Ticket");
         ticketC.addActionListener(this);
@@ -86,14 +78,9 @@ public class UI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if ( e.getActionCommand().equals("Book")) {
-            //BookingScreen();
-            p2.setBackground(Color.WHITE);
-            repaint();
-            System.out.println("hi");
+            BookingScreen();
         } else if ( e.getActionCommand().equals("Guest")) {
-            p2.setBackground(Color.LIGHT_GRAY);
-            repaint();
-            System.out.println(PlaneDB.isSeatBooked("201", 2));
+            GuestInfo();
         } else if ( e.getActionCommand().equals("Manual")) {
             DisplayManuel();
         } else if ( e.getActionCommand().equals("TicketCancel")) {
@@ -122,6 +109,36 @@ public class UI extends JFrame implements ActionListener {
     }
 
     /**
+     * Guest info.
+     */
+    public void GuestInfo(){
+        p2.setBackground(Color.WHITE);
+
+        //String flightcheck;
+        JLabel fc = new JLabel("What flight is being checked:");
+        JTextField ffield = new JTextField(3);
+
+        //int seatcheck;
+        JLabel sc = new JLabel("What seat number is being checked?");
+        JTextField stext = new JTextField(2);
+
+        //JButton
+
+        p2.add(fc);
+        p2.add(ffield);
+        p2.add(sc);
+        p2.add(stext);
+
+
+        //flightcheck = ffield.getText();
+        //String t = stext.getText();
+        //seatcheck = Integer.parseInt(t);
+        //repaint();
+
+        //System.out.println(PlaneDB.isSeatBooked(flightcheck,seatcheck));
+    }
+
+    /**
      * Display manuel.
      */
     public void DisplayManuel(){
@@ -132,13 +149,6 @@ public class UI extends JFrame implements ActionListener {
      * Display cancellation.
      */
     public void DisplayCancellation(){
-
-    }
-
-    /**
-     * Guest info.
-     */
-    public void GuestInfo(){
 
     }
 }
